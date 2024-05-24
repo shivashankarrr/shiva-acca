cd6b69dbadd8e63a2cc3bf7b3c832480e3bde000
shiva shankar <shankuu786@gmail.com>
2024-05-24 10:08:26 +0530

    shiva

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import BecomeAcca from './components/BecomeAcca'
+import WhyChooseUs from './components/WhyChooseUs'
+import LearnAcca from './components/LearnAcca'
+import PlacementAssistance from './components/PlacementAssistance'
+import KickOff from './components/KickOff'
+import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => (
+  <>
+    <Header />
+    <BecomeAcca />
+    <WhyChooseUs />
+    <LearnAcca />
+    <PlacementAssistance />
+    <KickOff />
+  </>
+)
+
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+const Header = () => (
+  <Navbar collapseOnSelect expand="lg" className="bg-primary">
+    <Container>
+      <Navbar.Brand href="#home">ACCA</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#whyChooseUs">why choose us</Nav.Link>
+          <Nav.Link href="#coursePlan">Course Plan</Nav.Link>
+          <Nav.Link href="#placementAssistance">Placement Assistance</Nav.Link>
+          <Nav.Link href="#accaJourney">Acca Journey</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+)
+
+export default Header
